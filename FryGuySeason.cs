ad816dd1958372bdeb2d61a8f88f97afb31d17f3
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-03-13 15:47:09 -0400

    Add files via upload

---
+++ FryGuySeason.cs
@@ -0,0 +1,37 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FryGuysMod
+{
+    public class FryGuySeason : ModSystem
+	{
+		public static bool FourthOfJulyActive { get; internal set; }
+        public static bool CalendarFourthOfJuly;
+        public static bool CalendarXMas;
+        public static bool CalendarHalloween;
+
+        public override void PostUpdateWorld()
+        {
+            if (CalendarFourthOfJuly)
+            {
+                FourthOfJulyActive = true;
+            }
+            else if (Main.time == 0 && Main.dayTime && Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                FourthOfJulyActive = DateTime.Now.Month == 7;
+            }
+
+            if (CalendarXMas)
+            {
+                Main.xMas = true;
+            }
+
+            if (CalendarHalloween)
+            {
+                Main.halloween = true;
+            }
+        }
+    }
+}
---
+++ FryGuysMod.cs
@@ -0,0 +1,16 @@
+using Terraria.ModLoader;
+using FryGuysMod.Common;
+
+namespace FryGuysMod
+{
+	public class FryGuysMod : Mod
+	{
+        public override void Load()
+        {
+            FryGlobalItem.Sets.Initialize();
+            FryGuySeason.CalendarFourthOfJuly = false;
+            FryGuySeason.CalendarHalloween = false;
+            FryGuySeason.CalendarXMas = false;
+        }
+    }
+}
