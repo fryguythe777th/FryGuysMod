874c68659b60d76d9d403ac9bffffe4101d01ce3
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:19:56 -0400

    Add files via upload

---
+++ Content/Items/FourthOfJulyItem.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace FryGuysMod.Content.Items
+{
+	public abstract class FourthOfJulyItem : ModItem
+	{
+		public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+			var line = new TooltipLine(Mod, "FourthOfJulyItem:Indicator", "Fourth of July Item")
+			{
+				OverrideColor = new Color(50, 0, 255)
+			};
+			tooltips.Add(line);
+        }
+	}
+}
