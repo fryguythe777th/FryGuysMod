89dc37f573b46473ceaa341623c31b9b6cfadd26
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:16:15 -0400

    stuff

---
+++ Content/NPCs/Critters/PatriotBunny.cs
@@ -0,0 +1,67 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using FryGuysMod.Content.Biomes;
+using FryGuysMod.Common;
+using Terraria.GameContent.Events;
+using static Terraria.GameContent.RGB.CommonConditions;
+using FryGuysMod.Content.Items.Weapons;
+using Terraria.GameContent.Personalities;
+using Terraria.GameContent.Bestiary;
+
+namespace FryGuysMod.Content.NPCs.Critters
+{
+	public class PatriotBunny : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bunny");
+
+			Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Bunny];
+			Main.npcCatchable[Type] = true;
+
+			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0)
+			{
+				Velocity = 1f
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+
+				new BestiaryPortraitBackgroundProviderPreferenceInfoElement(BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.DayTime),
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.DayTime,
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface,
+				new FlavorTextBestiaryInfoElement("This cute little bunny felt a little patriotic before going out today and put on a fancy hat.")
+
+			});
+		}
+
+		public override void SetDefaults()
+        {
+			NPC.CloneDefaults(NPCID.Bunny);
+			AIType = NPCID.Bunny;
+			NPC.catchItem = ItemID.Bunny;
+			AnimationType = NPCID.Bunny;
+		}
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+			if (FryGuySeason.FourthOfJulyActive == true && Main.LocalPlayer.ZoneForest == true && Main.dayTime)
+            {
+				return 0.1f;
+            }
+			else
+            {
+				return 0f;
+            }
+        }
+    }
+}
---
+++ Content/NPCs/PatriotDemonEye.cs
@@ -0,0 +1,59 @@
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using FryGuysMod;
+
+namespace FryGuysMod.Content.NPCs
+{
+	public class PatriotDemonEye : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Demon Eye");
+
+			Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.DemonEye];
+
+			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0)
+			{
+				Velocity = 1f,
+				PortraitPositionYOverride = 5f
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+
+				new BestiaryPortraitBackgroundProviderPreferenceInfoElement(BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime),
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime,
+                
+				//new SpawnConditionBestiaryInfoElement("Fourth of July", (Texture2D)TextureAddressMode(FryGuysMod/icon_small), null, )
+				new FlavorTextBestiaryInfoElement("This Demon Eye has had the unfortunate experience of having a firework shoved in its eye. Unfortunately for you, it can still see well enough to attack you.")
+
+			});
+		}
+
+		public override void SetDefaults()
+        {
+			NPC.CloneDefaults(NPCID.DemonEye);
+			NPC.width = 44;
+			AIType = NPCID.DemonEye;
+			AnimationType = NPCID.DemonEye;
+		}
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+			if (FryGuySeason.FourthOfJulyActive == true && !Main.dayTime && Main.LocalPlayer.ZoneOverworldHeight == true)
+            {
+				return 0.1f;
+            }
+			else
+            {
+				return 0f;
+            }
+        }
+    }
+}
