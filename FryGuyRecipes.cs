9427259785a061f29e3970bb8068e998e53c5fb6
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:12:04 -0400

    fry gus recipe

---
+++ FryGuyRecipes.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace FryGuysMod
+{
+	public class FryGuyRecipes : ModSystem
+	{
+		public static RecipeGroup AnyEvilComponent;
+
+		public override void AddRecipeGroups()
+        {
+			AnyEvilComponent = new RecipeGroup(() => "Any Evil Component",
+				ItemID.ShadowScale, ItemID.TissueSample);
+
+			RecipeGroup.RegisterGroup("[i:{86}]", AnyEvilComponent);
+        }
+	}
+}
