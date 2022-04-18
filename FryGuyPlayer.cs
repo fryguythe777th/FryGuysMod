3dba4c0fd875cd2462a6eb2df85acda8eaf76a34
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:12:33 -0400

    fryguy playa

---
+++ FryGuyPlayer.cs
@@ -0,0 +1,99 @@
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace FryGuysMod
+{
+	public class FryGuyPlayer : ModPlayer
+	{
+        public bool PyrotechnicBadge;
+		public bool SoldierHat;
+
+        public override void ResetEffects()
+        {
+			PyrotechnicBadge = false;
+			SoldierHat = false;
+        }
+
+        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
+        {
+			if(PyrotechnicBadge == true)
+            {
+				var source = target.GetSpawnSource_ForProjectile();
+				Vector2 velocity = new Vector2(0, 0);
+				var fireworkChance = WorldGen.genRand.Next(5);
+
+				if (fireworkChance == 0)
+				{
+					switch (WorldGen.genRand.Next(4))
+					{
+						case 0:
+							int b = Projectile.NewProjectile(source, target.Center, velocity, ProjectileID.RocketFireworkBlue, damage, knockback, Player.whoAmI, 0, 1);
+							Main.projectile[b].timeLeft = 1;
+							Main.projectile[b].hide = true;
+							return;
+
+						case 1:
+							int r = Projectile.NewProjectile(source, target.Center, velocity, ProjectileID.RocketFireworkRed, damage, knockback, Player.whoAmI, 0, 1);
+							Main.projectile[r].timeLeft = 1;
+							Main.projectile[r].hide = true;
+							return;
+
+						case 2:
+							int g = Projectile.NewProjectile(source, target.Center, velocity, ProjectileID.RocketFireworkGreen, damage, knockback, Player.whoAmI, 0, 1);
+							Main.projectile[g].timeLeft = 1;
+							Main.projectile[g].hide = true;
+							return;
+
+						case 3:
+							int y = Projectile.NewProjectile(source, target.Center, velocity, ProjectileID.RocketFireworkYellow, damage, knockback, Player.whoAmI, 0, 1);
+							Main.projectile[y].timeLeft = 1;
+							Main.projectile[y].hide = true;
+							return;
+					}
+				}
+			}
+        }
+
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+			if(PyrotechnicBadge == true)
+            {
+				var source = proj.GetProjectileSource_FromThis();
+				Vector2 velocity = new Vector2(0, 0);
+				var fireworkChance = WorldGen.genRand.Next(10);
+
+				if (fireworkChance == 0)
+				{
+					switch (WorldGen.genRand.Next(4))
+					{
+						case 0:
+							int b = Projectile.NewProjectile(source, proj.Center, velocity, ProjectileID.RocketFireworkBlue, damage, knockback, proj.owner, 0, 1);
+							Main.projectile[b].timeLeft = 1;
+							Main.projectile[b].hide = true;
+							return;
+
+						case 1:
+							int r = Projectile.NewProjectile(source, proj.Center, velocity, ProjectileID.RocketFireworkRed, damage, knockback, proj.owner, 0, 1);
+							Main.projectile[r].timeLeft = 1;
+							Main.projectile[r].hide = true;
+							return;
+
+						case 2:
+							int g = Projectile.NewProjectile(source, proj.Center, velocity, ProjectileID.RocketFireworkGreen, damage, knockback, proj.owner, 0, 1);
+							Main.projectile[g].timeLeft = 1;
+							Main.projectile[g].hide = true;
+							return;
+
+						case 3:
+							int y = Projectile.NewProjectile(source, proj.Center, velocity, ProjectileID.RocketFireworkYellow, damage, knockback, proj.owner, 0, 1);
+							Main.projectile[y].timeLeft = 1;
+							Main.projectile[y].hide = true;
+							return;
+					}
+				}
+			}
+		}
+    }
+}
