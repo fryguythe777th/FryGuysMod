d75f621b025ecaa6978d904cc1e86f06367adb73
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:03:53 -0400

    fryguy projectile

---
+++ Common/FryGuyProjectile.cs
@@ -0,0 +1,45 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using System;
+using FryGuysMod.Content.Items.Weapons.Engineer;
+
+namespace FryGuysMod.Common
+{
+	public class FryGuyProjectile : GlobalProjectile
+	{
+        public override bool PreAI(Projectile projectile)
+        {
+            Player player = Main.player[Main.myPlayer];
+            FryGuyPlayer modPlayer = player.GetModPlayer<FryGuyPlayer>();
+
+            if(modPlayer.SoldierHat == true && projectile.friendly && projectile.DamageType == DamageClass.Ranged)
+            {
+                projectile.velocity *= 1.10f;
+            }
+
+            return true;
+        }
+
+        public override bool OnTileCollide(Projectile projectile, Vector2 oldVelocity)
+        {
+            if (projectile.type == ProjectileID.Flames)
+            {
+                FryGuyMethods.CheckDestructibility(1, oldVelocity, projectile);
+
+                return false;
+            }
+            else if (projectile.type == ModContent.ProjectileType<DemonTongueTip>())
+            {
+                FryGuyMethods.CheckDestructibility(5, oldVelocity, projectile);
+
+                return true;
+            }
+            else
+            {
+                return projectile.tileCollide;
+            }
+        }
+    }
+}
