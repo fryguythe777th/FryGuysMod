6e1022d41ae5421285a0fb9216212fb1192c7383
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 17:26:50 -0400

    drip shoes

---
+++ Content/Items/Accessories/Boots/SuperBoots.cs
@@ -0,0 +1,43 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using FryGuysMod.Content.Items.Accessories.Boots;
+using Terraria.GameContent.Creative;
+
+namespace FryGuysMod.Content.Items.Accessories.Boots
+{
+    public class SuperBoots : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Increased acceleration\n" +
+				"30% increased movement speed");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.accessory = true;
+            Item.value = Item.sellPrice(gold: 3);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.runAcceleration *= 1.20f;
+            player.moveSpeed *= 1.30f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.ChlorophyteBar, 4)
+                .AddIngredient(ItemID.SpectreBar, 4)
+                .AddIngredient(ItemID.ShroomiteBar, 4)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
