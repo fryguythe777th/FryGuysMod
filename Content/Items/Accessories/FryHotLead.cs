ca4dc7d28dbf9e83a8281e1bba7cd672941d81ab
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-04-18 21:31:14 -0400

    new accessories

---
+++ Content/Items/Accessories/FryHotLead.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using FryGuysMod;
+using Terraria.GameContent.Creative;
+
+namespace FryGuysMod.Content.Items.Accessories
+{
+    public class FryHotLead : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hot Lead");
+
+            Tooltip.SetDefault("Ranged attacks inflict fire to enemies.");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<FryGuyPlayer>().HotLead = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.LeadBar, 6)
+                .AddIngredient(ItemID.MagmaStone)
+                .AddIngredient(ModContent.GetInstance<Gunpowder>(), 2)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Items/Accessories/FryRiddleOfLead.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using FryGuysMod;
+using Terraria.GameContent.Creative;
+
+namespace FryGuysMod.Content.Items.Accessories
+{
+    public class FryRiddleOfLead : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Riddle of Lead");
+
+            Tooltip.SetDefault("Increased max life, damage, and speed.\n" +
+                "Chance to gain additional invincibility frames on hit when under 1/6 of your max life.");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<FryGuyPlayer>().RiddleOfLead = true;
+            player.statLifeMax += 30;
+            player.GetDamage(DamageClass.Generic) *= 1.10f;
+            player.moveSpeed *= 1.10f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.LeadBar, 6)
+                .AddIngredient(ItemID.HallowedBar, 4)
+                .AddIngredient(ModContent.GetInstance<Gunpowder>(), 2)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
