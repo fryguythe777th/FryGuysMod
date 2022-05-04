e3023e26990989d1a2f6ce06f44f39bce2fc8eef
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2022-05-04 08:49:16 -0400

    Update EngineerEmblem.cs

--- Content/Items/Accessories/Engineer/EngineerEmblem.cs
+++ Content/Items/Accessories/Engineer/EngineerEmblem.cs
@@ -1,15 +1,18 @@
 using FryGuysMod.DamageClasses;
 using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace FryGuysMod.Content.Items.Accessories.Engineer
 {
-    public class EngineerEmblem : ModItem
+    public class EngineerCap : ModItem
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("15% increased engineer damage");
+            Tooltip.SetDefault("5% increased engineer damage");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
         public override void SetDefaults()
@@ -17,22 +20,21 @@ namespace FryGuysMod.Content.Items.Accessories.Engineer
             Item.width = 28;
             Item.height = 28;
             Item.accessory = true;
-
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.buyPrice(gold: 10);
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetDamage<EngineerDamage>() *= 1.15f;
+            player.GetDamage<EngineerDamage>() *= 1.05f;
         }
 
         public override void AddRecipes()
         {
-            Mod.CreateRecipe(ItemID.AvengerEmblem)
-                .AddIngredient<EngineerEmblem>()
-                .AddIngredient(ItemID.SoulofMight, 5)
-                .AddIngredient(ItemID.SoulofFright, 5)
-                .AddIngredient(ItemID.SoulofSight, 5)
-                .AddTile(TileID.TinkerersWorkbench)
+            CreateRecipe()
+                .AddIngredient(ItemID.FlinxFur, 5)
+                .AddIngredient(ItemID.MeteoriteBar, 5)
+                .AddTile(TileID.Anvils)
                 .Register();
         }
     }
